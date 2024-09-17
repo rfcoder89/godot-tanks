5daa6a5105f70510f13a41b1340d1cf4c298f720
Richard Flood <rfcoder89@gmail.com>
2024-09-17 20:30:42 +0100

    basic mouse controls (y axis, no capture)

--- Tanks/player_tank.gd
+++ Tanks/player_tank.gd
@@ -5,10 +5,20 @@ extends Node3D
 var moveForwards : bool = false
 var moveBackwards : bool = false
 
+var cameraDistance : float = 2;
+var cameraYAngle : float = 0;
+var cameraXAngle : float = 0;
+
+@export var mouseInput : Vector2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var viewport_transform: Transform2D = get_tree().root.get_final_transform()
+		mouseInput += event.xformed_by(viewport_transform).relative
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -16,13 +26,22 @@ func _process(delta: float) -> void:
 		return
 		
 	if camera.get_parent() != $CameraAnchor:
-		print(camera.transform.origin)
 		camera.reparent($CameraAnchor)
 		camera.transform.origin = Vector3.ZERO
-		print(camera.transform.origin)
-		# camera.transform.origin = Vector3.ZERO
 		
-	$CameraAnchor.look_at($Tank/CameraTarget.transform.origin)
+	# print(mouseInput)
+	cameraYAngle += mouseInput.x * delta * 0.3
+	while cameraYAngle < 0:	  cameraYAngle += TAU
+	while cameraYAngle > TAU: cameraYAngle -= TAU
+	
+	mouseInput = Vector2.ZERO
+	
+	#$CameraAnchor.transform.origin = Vector3(-1, 0, 0)
+	var cat : Transform3D = Transform3D()  # camera anchor transform
+	cat = cat.translated(Vector3(6, 2.5, 0))
+	cat = cat.rotated(Vector3(0, 1, 0), -cameraYAngle)
+	$CameraAnchor.transform.origin = cat.origin
+	$CameraAnchor.look_at(to_global(Vector3(0, 2, 0)))
 	
 	if (Input.is_action_pressed("Move forward") and !moveForwards):
 		moveForwards = true
