874499b14eb33c21040b0cd20c65b7d9c590f13b
Richard Flood <rfcoder89@gmail.com>
2024-09-17 13:15:15 +0100

    working with transforms
    camera positioned correctly..ish

--- Tanks/player_tank.gd
+++ Tanks/player_tank.gd
@@ -1,8 +1,9 @@
 extends Node3D
 
 @export var camera : Camera3D = null
-@export var moveForwards : bool = false
-@export var moveBackwards : bool = false
+@export var control : bool = false;
+var moveForwards : bool = false
+var moveBackwards : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,11 +12,24 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if (Input.is_action_pressed("Move forward")):
+	if !control:
+		return
+		
+	if camera.get_parent() != $CameraAnchor:
+
+		print(camera.transform.origin)
+		camera.reparent($CameraAnchor)
+		camera.transform.origin = Vector3.ZERO
+		print(camera.transform.origin)
+		# camera.transform.origin = Vector3.ZERO
+		
+	#$CameraAnchor.look_at($Tank.transform.origin)
+	
+	if (Input.is_action_pressed("Move forward") and !moveForwards):
 		moveForwards = true
 		moveBackwards = false;
 		
-	if (Input.is_action_pressed("Move backwards")):
+	if (Input.is_action_pressed("Move backwards") and !moveBackwards):
 		moveForwards = false
 		moveBackwards = true;		
 
