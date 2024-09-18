5923aaf4a2e8f5e456e80a7aa3fda54a8b0d8eb3
Richard Flood <rfcoder89@gmail.com>
2024-09-18 21:22:21 +0100

    rotate turret left + right (q + e)

--- Tanks/player_tank.gd
+++ Tanks/player_tank.gd
@@ -5,12 +5,15 @@ extends Node3D
 
 var moveDir : int = 0
 var rotateDir: int = 0
+var rotateTurretDir: int = 0;
 
 var cameraDistance : float = 2;
 var cameraYAngle : float = 0;
 var turretAngle : float = 0;
 
 @onready var cameraAnchor : Node3D = $CameraAnchor
+@onready var rb : RigidBody3D = $RigidBody3D
+
 @export var mouseInput : Vector2
 
 func _ready() -> void:
@@ -22,37 +25,35 @@ func _unhandled_input(event: InputEvent) -> void:
 		mouseInput += event.xformed_by(viewport_transform).relative
 
 func _physics_process(delta: float) -> void:
-	var localDir = ($RigidBody3D.basis * Vector3(1, 0, 0)).normalized()
+	var localDir = (rb.basis * Vector3(1, 0, 0)).normalized()
 	if moveDir == 0:
-		var lv = $RigidBody3D.linear_velocity
-		$RigidBody3D.linear_velocity *= 0.8
-		$RigidBody3D.constant_force = Vector3.ZERO
-		#var brakeForce : Vector3 = lv * -0.3;
-		#brakeForce.y = 0;
-		#$RigidBody3D.add_constant_central_force(brakeForce)
+		var lv = rb.linear_velocity
+		var yVel : float = rb.linear_velocity.y;
+		rb.linear_velocity *= 0.8;
+		rb.linear_velocity.y = yVel;
+		rb.constant_force = Vector3.ZERO
 	else:
 		var maxSpeed = 6;
-		print($RigidBody3D.linear_velocity)
-		if $RigidBody3D.linear_velocity.length() < maxSpeed:
-			$RigidBody3D.constant_force = localDir * 30.8 * moveDir
+		print(rb.linear_velocity)
+		if rb.linear_velocity.length() < maxSpeed:
+			rb.constant_force = localDir * 30.8 * moveDir
 		else:
-			$RigidBody3D.linear_velocity = $RigidBody3D.linear_velocity.limit_length(maxSpeed)
-			$RigidBody3D.constant_force = Vector3.ZERO
+			rb.linear_velocity = rb.linear_velocity.limit_length(maxSpeed)
+			rb.constant_force = Vector3.ZERO
 			
 	if rotateDir == 0:
-		$RigidBody3D.angular_velocity *= 0.8
-		$RigidBody3D.constant_torque = Vector3.ZERO
+		rb.angular_velocity *= 0.8
+		rb.constant_torque = Vector3.ZERO
 	else:
-		#print($RigidBody3D.angular_velocity)
 		var maxSpeed = 6;
-		if abs($RigidBody3D.angular_velocity.y) < maxSpeed:
-			$RigidBody3D.constant_torque = Vector3(0, 20.8 * -rotateDir, 0)
-			print($RigidBody3D.constant_torque)
+		if abs(rb.angular_velocity.y) < maxSpeed:
+			rb.constant_torque = Vector3(0, 20.8 * -rotateDir, 0)
+			print(rb.constant_torque)
 		else:
-			$RigidBody3D.angular_velocity = Vector3(
-				$RigidBody3D.angular_velocity.x,
-				max(-maxSpeed, min(maxSpeed, $RigidBody3D.angular_velocity.y)),
-				$RigidBody3D.angular_velocity.z
+			rb.angular_velocity = Vector3(
+				rb.angular_velocity.x,
+				max(-maxSpeed, min(maxSpeed, rb.angular_velocity.y)),
+				rb.angular_velocity.z
 			)	
 
 func _process(delta: float) -> void:
@@ -69,12 +70,14 @@ func _process(delta: float) -> void:
 	mouseInput = Vector2.ZERO
 	
 	var cat : Transform3D = Transform3D()  # camera anchor transform
-	cat = cat.translated(Vector3(7, 2.5, 0))
+	cat = cat.translated(Vector3(-7, 2.5, 0))
 	cat = cat.rotated(Vector3(0, 1, 0), -cameraYAngle)
-	cat = cat.translated($RigidBody3D.transform.origin)
-	
+	cat = cat.translated(rb.transform.origin)	
 	cameraAnchor.transform.origin = cat.origin
-	cameraAnchor.look_at($RigidBody3D.transform.origin + Vector3(0, 2, 0))
+	cameraAnchor.look_at(rb.transform.origin + Vector3(0, 2, 0))
+	
+	turretAngle += rotateTurretDir * -5 * delta;
+	$RigidBody3D/Tank/TanksTurretMesh001.set_rotation(Vector3(0, turretAngle, 0))
 	
 	if Input.is_action_pressed("Move forward"):
 		moveDir = 1
@@ -89,3 +92,10 @@ func _process(delta: float) -> void:
 		rotateDir = -1
 	else:
 		rotateDir = 0
+		
+	if Input.is_action_pressed("Rotate turret right"):
+		rotateTurretDir = 1
+	elif Input.is_action_pressed("Rotate turret left"):
+		rotateTurretDir = -1
+	else:
+		rotateTurretDir = 0
